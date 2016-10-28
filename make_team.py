c6495a38ac9854ee3011edc9e8c52aeb6bec32b8
Juhyeun Han <hjh1019@gmail.com>
2016-10-28 16:29:49 +0900

    added team member modified minor bugs

--- make_team.py
+++ make_team.py
@@ -4,20 +4,23 @@ import random
 def make_team(member_list, num_team):
 	num_member_of_team = len(member_list) / num_team
 	remainder = len(member_list) % num_team
-	random.shuffle(people)
+	random.shuffle(member_list)
 	teams = []
 	for i in range(num_team):
-		team = people[i * num_member_of_team : i * num_member_of_team + num_member_of_team]
+		team = member_list[i * num_member_of_team : i * num_member_of_team + num_member_of_team]
 		teams.append(team)
 	if not remainder == 0:
 		for i in range(remainder):
-			teams[i].append(people[num_member_of_team * num_team + i])
-	for i in range(len(teams)):
-		random.shuffle(teams[i])
-
+			teams[i].append(member_list[num_member_of_team * num_team + i])
 	return teams
 
-people = ['김상훈', '김세영', '김하윤', '김한샘', '나상일', '박지민', '이경옥', '이수빈', '이지민', '장언동', '장정운', '최찬규', '최호열', '한주현']
-teams = make_team(people, 4)
-for i in range(len(teams)):
+people1 = ['김상훈', '김세영', '김하윤', '김한샘', '나상일', '박지민', '이경옥', '이수빈', '이지민', '장언동', '장정운', '최찬규', '최호열', '한주현']
+people2 = ['김혜선', '오종민', '이선명', '이지아', '이현균', '이화옥', '정유진', '주슬기', '진달래', '최기정', '최유정']
+teams1 = make_team(people1, 4)
+teams2 = make_team(people2, 4)
+teams = []
+for i in range(len(teams1)):
+	teams.append(teams1[i] + teams2[len(teams2) - i - 1])
+	# To select team leader
+	random.shuffle(teams[i])
 	print " ".join(teams[i])
